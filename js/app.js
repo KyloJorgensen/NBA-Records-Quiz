1f40335bf03dc3505770e29919ee2132fd69d2c7
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-06-06 21:25:41 -0700

    changes the question objects into array in the vars. changed the button handlers. rewrote most of the code.

--- js/app.js
+++ js/app.js
@@ -1,174 +1,147 @@
 $(document).ready(function() {
-	var score = 0,
-		questionCount = 0,
-		question; 
-		questionObjects = {
-		question: '',
-		anwser: '',
-		falseAnwser1: '',
-		falseAnwser2: '',
-		falseAnwser3: '',
-		anwserComment: ''
-	}
-		firstQ = Object.create(questionObjects),
-		secondQ = Object.create(questionObjects),
-		thirdQ = Object.create(questionObjects),
-		forthQ = Object.create(questionObjects),
-		fifthQ = Object.create(questionObjects);
+    var score = 0,
+        currentQuestion = -1,
+        questions = [
+            { question: 'Who holds NBA career record for personal fouls?', anwser: 'Kareem Abdul-Jabbar', chocies: ['Kareem Abdul-Jabbar', 'Karl Malone ', 'Robert Parish', 'Charles Oakley'], anwserComment: 'Kareem Abdul-Jabbar holds the NBA career record with 4657 personal fouls.' },
+            { question: 'Who holds the NBA career record for steals?', anwser: 'John Stockton', chocies: ['John Stockton', 'Jason Kidd', 'Michael Jordan', 'Gary Payton'], anwserComment: 'John Stockton holds the NBA career record with 3265 steals.' },
+            { question: 'Who holds NBA career records for 3-Pt field goals?', anwser: 'Ray Allen', chocies: ['Ray Allen', 'Reggie Miller', 'Jason Terry', 'Paul Pierce'], anwserComment: 'Ray Allen holds the NBA career record with 2973 3-pt field goals.' },
+            { question: 'Who holds NBA career records for games played?', anwser: 'Robert Parish', chocies: ['Robert Parish', 'Kareem Abdul-Jabbar', 'John Stockton', 'Karl Malone'], anwserComment: 'Robert Parish holds the NBA career record with 1611 games played.' },
+            { question: 'Who holds NBA career records for blocks?', anwser: 'Hakeem Olajuwon', chocies: ['Hakeem Olajuwon', 'Dikembe Mutombo', 'Kareem Abdul-Jabbar', 'Mark Eaton'], anwserComment: 'Hakeem Olajuwon holds the NBA career record with 3830 blocks.' }
+        ];
 
-	firstQ.question = 'Who holds NBA career record for personal fouls?';
-	firstQ.anwser = 'Kareem Abdul-Jabbar';
-	firstQ.falseAnwser1 = 'Karl Malone	';
-	firstQ.falseAnwser2 = 'Robert Parish';
-	firstQ.falseAnwser3 = 'Charles Oakley';
-	firstQ.anwserComment = 'Kareem Abdul-Jabbar holds the NBA career record with 4657 personal fouls.';
+    //Checks if the button in main has been clicked
+    $('main').on('click', 'button', function() {
+        buttonPushed();
+    });
 
-	secondQ.question = 'Who holds the NBA career record for steals?';
-	secondQ.anwser = 'John Stockton';
-	secondQ.falseAnwser1 = 'Jason Kidd';
-	secondQ.falseAnwser2 = 'Michael Jordan';
-	secondQ.falseAnwser3 = 'Gary Payton';
-	secondQ.anwserComment = 'John Stockton holds the NBA career record with 3265 steals.';
-
-	thirdQ.question = 'Who holds NBA career records for 3-Pt field goals?';
-	thirdQ.anwser = 'Ray Allen';
-	thirdQ.falseAnwser1 = 'Reggie Miller';
-	thirdQ.falseAnwser2 = 'Jason Terry';
-	thirdQ.falseAnwser3 = 'Paul Pierce';
-	thirdQ.anwserComment = 'Ray Allen holds the NBA career record with 2973 3-pt field goals.';
-
-	forthQ.question = 'Who holds NBA career records for games played?';
-	forthQ.anwser = 'Robert Parish';
-	forthQ.falseAnwser1 = 'Kareem Abdul-Jabbar';
-	forthQ.falseAnwser2 = 'John Stockton';
-	forthQ.falseAnwser3 = 'Karl Malone';
-	forthQ.anwserComment = 'Robert Parish holds the NBA career record with 1611 games played.';
-
-	fifthQ.question = 'Who holds NBA career records for blocks?';
-	fifthQ.anwser = 'Hakeem Olajuwon';
-	fifthQ.falseAnwser1 = 'Dikembe Mutombo';
-	fifthQ.falseAnwser2 = 'Kareem Abdul-Jabbar';
-	fifthQ.falseAnwser3 = 'Mark Eaton';
-	fifthQ.anwserComment = 'Hakeem Olajuwon holds the NBA career record with 3830 blocks.';
-
-	var questionsArray = [firstQ, secondQ, thirdQ, forthQ, fifthQ]
+    // chages which anwserthe users is chooseing
+    $('body').on('click', 'i', function() {
+        $('i').removeClass('icon-check');
+        $('i').addClass('icon-check-empty');
+        $(this).removeClass('icon-check-empty');
+        $(this).addClass('icon-check');
+    })
+    // shows help box
+    .on('click', '.help', function() {
+        $('footer').show();
+        $(this).hide();
+    })
+    // hides helpbox
+    .on('click', 'footer', function() {
+        $('.help').show();
+        $(this).hide();
+    });
 
 
-	//Changes page
-	$('body').on('click', '.start', function() {
-		nextQuestion();
-		$('button').text('SUBMIT')
-		.removeClass('start')
-		.addClass('submit');
-	})
-	.on('click', '.submit', function() {
-		verifySelection();
-	})
-	.on('click', '.next', function() {
-		nextQuestion();
-		$('button').text('SUBMIT')
-		.removeClass('next')
-		.addClass('submit');
-		if (questionCount == 6) {
-			gameOver();
-		}
-	})
-	.on('click', '.tryagain', function() {
-		newGame();
-		$('button').text('START')
-		.removeClass('tryagain')
-		.addClass('start');
-	})
-	.on('click', '.icon-check-empty', function(){
-		$('i').removeClass('icon-check');
-		$('i').addClass('icon-check-empty');
-		$(this).removeClass('icon-check-empty');
-		$(this).addClass('icon-check');
-	})
-	.on('click', '.help', function() {
-		$('footer').show();
-		$(this).hide();
-	})
-	.on('click', 'footer', function () {
-		$('.help').show();
-		$(this).hide();
-	});
+    //determines what to do when button is push
+    function buttonPushed() {
+        if ($('main').children('button').hasClass('next')) {
+			currentQuestion++;
+            if (getNextQuestion()) {
+            	$('#currentQuestion').text(currentQuestion + 1);
+                changeButton('next', 'submit');
+            } else {
+                gameOver();
+                changeButton('next', 'tryagain');
+            }
 
-	//starts a new game
-	function newGame() {
-		score = 0
-		$('#score').text(score);
-		questionCount = 0;
-		$('#questioncounter').text('0');
-		$('.textbox').text('READY TO PLAY!!');
-		$('button').text('START');
-		$('.end').hide();
-	}
+        } else if ($('main').children('button').hasClass('submit')) {
+            if (valifySelection()) {
+                validateAnwser();
+                getAnwserComment();
+                changeButton('submit', 'next');
+                
+            }
+        } else if ($('main').children('button').hasClass('tryagain')) {
+            newGame();
+            changeButton('tryagain', 'next');
+        }
+    }
 
-	// gets next question page
-	function nextQuestion() {
-		question = questionsArray[questionCount];
-		questionCount++;
-		if (questionCount < 6) { 
-			$('#questioncounter').text(questionCount);
-			$('.textbox').text(question.question);
-			var awnsers = [question.anwser, question.falseAnwser1, question.falseAnwser2, question.falseAnwser3];
-			awnsers = shuffle(awnsers);
-			$('ul').empty();
-			for (var i = 0; i < awnsers.length; i++) {
-				$('ul').append('<li><i class="icon-check-empty"></i><p>' + awnsers[i] + '</p></li>')
-			}
-			$('button').text('SUBMIT');
-		}
-		
-	}
-	//shuffles arrays
-	function shuffle(array) {
-	  var currentIndex = array.length, temporaryValue, randomIndex;
+    // gets next question 
+    function getNextQuestion() {
+        var returnval = true;
+        if (currentQuestion < questions.length) {
+            $('.textbox').text(questions[currentQuestion].question);
+            anwsers = shuffle(questions[currentQuestion].chocies);
+            $('ul').empty();
+            for (var i = 0; i < anwsers.length; i++) {
+                $('ul').append('<li><i class="icon-check-empty"></i><p>' + anwsers[i] + '</p></li>')
+            }
+        } else {
+            returnval = false;
+        }
+        return returnval;
+    }
 
-	  // While there remain elements to shuffle...
-	  while (0 !== currentIndex) {
+    //shuffles arrays
+    function shuffle(array) {
+        var currentIndex = array.length,
+            temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
 
-	    // Pick a remaining element...
-	    randomIndex = Math.floor(Math.random() * currentIndex);
-	    currentIndex -= 1;
+        return array;
+    }
 
-	    // And swap it with the current element.
-	    temporaryValue = array[currentIndex];
-	    array[currentIndex] = array[randomIndex];
-	    array[randomIndex] = temporaryValue;
-	  }
+    // changes button in main
+    function changeButton(currentButton, newbutton) {
+        $('main').children('button').text(newbutton).removeClass(currentButton).addClass(newbutton);
+        if (currentButton == 'tryagain') {
+ 			$('main').children('button').text('start')
+    	}
+    }
 
-	  return array;
-	}
+    // shows score and butto to try again.
+    function gameOver() {
+        $('.textbox').text('YOUR SCORE: ' + score)
+        $('ul').empty();
+        $('ul').append('<li><p>Thanks for playing!</p></li>')
+    }
 
-	//verifies if the user has selected a anwser
-	function verifySelection() {
-		$('i').hasClass('icon-check') ? anwserPage() : alert('Please select your anwser.');
-	}
+    //verifies if the user has selected a anwser
+    function valifySelection() {
+        var returnval = true
+        if ($('i').hasClass('icon-check') == false) {
+            alert('Please select your anwser.');
+            returnval = false;
+        }
+        return returnval;
+    }
 
-	function anwserPage() {
-		$('.icon-check').siblings('p').html() == question.anwser ? scored() : $('.textbox').text('Sorry to got it wrong.');
-		$('ul').empty();
-		$('ul').append('<li><p>' + question.anwserComment + '</p></li>')
-		$('button').text('NEXT')
-		.removeClass('submit')
-		.addClass('next');
-	}
+    //vaildates users anwser
+    function validateAnwser() {
+    	console.log($('.icon-check').siblings('p').html());
+    	console.log(questions[currentQuestion].anwser);
+        if ($('.icon-check').siblings('p').html() == questions[currentQuestion].anwser) {
+            $('.textbox').text('YOU GOT IT RIGHT!!');
+            score++;
+            $('#score').text(score);
+        } else {
+            $('.textbox').text('Sorry got that one wrong.');
+        }
+    }
 
-	function scored() {
-		$('.textbox').text('YOU GOT IT RIGHT!!');
-		score ++;
-		$('#score').text(score);
-	}
+    // gets Awnser comment
+    function getAnwserComment() {
+        $('ul').empty();
+        $('ul').append('<li><p>' + questions[currentQuestion].anwserComment + '</p></li>')
+    }
 
-	function gameOver() {
-		$('.textbox').text('YOUR SCORE: ' + score)
-		$('ul').empty();
-		$('ul').append('<li><p>Thanks for playing!</p></li>')
-		$('button').text('TRYAGAIN')
-		.removeClass('submit')
-		.addClass('tryagain');
-		$('ul').empty();
-	}
+    //starts a new game
+    function newGame() {
+        score = 0;
+        $('#score').text(score);
+        currentQuestion = -1;
+        $('#currentQuestion').text(currentQuestion + 1);
+        $('.textbox').text('READY TO PLAY!!');
+        $('button').text('START');
+        $('ul').empty();
+    }
+
 });
